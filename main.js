213307c8ccc23be74ea09eab3f29e1e2b8292def
Hermine <95001028+Tkharkhelauri@users.noreply.github.com>
2024-03-04 23:35:11 +0400

    experimented with different js commands

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ slider.addEventListener('mousemove', function () {
 		'linear-gradient(90deg, var(--col-green)' + x * 5 + '%, var(--col-black)' + x * 5 + '%)'
 	slider.style.background = color
 })
+
+// const passwordBox = document.getElementById("password");
+// const lenght = 12;
+// const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+// const number = "0123456789";
+// const symbol = "@#$ %^&* ( ) _+~|}{[]></-=";
+// function createPassword(){
+// let password =
+// 11 11
+// password += upperCase [Math.floor(Math.random()* upperCase.length)];
+// password += lowerCase [Math.floor (Math.random() * lowerCase.length)];
+// password += number [Math.floor(Math.random() * number.length)];
+// password += symbol [Math.floor(Math.random() * symbol.length)];
+// }
